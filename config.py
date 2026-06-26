8d635709bcd21d68b6040aac2486985976c03309
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2026-06-27 00:50:20 +0530

    Update auto delete timer in config.py
    Increased auto delete timer from 300 to 600 seconds.

--- config.py
+++ config.py
@@ -24,14 +24,14 @@ DB_NAME = "yato"
 
 FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL =  "-1004472965009"  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
 #     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
 # Auto Delete Timer (seconds)
-AUTO_DEL = 300
+AUTO_DEL = 600
 # Admin IDs
 ADMINS = [6497757690, 6103092779]
 # Bot Settings
