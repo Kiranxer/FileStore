f92eef49f253ebfbd90700a2ecac501904dba510
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2026-06-27 01:01:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "Cluster0"
 
 FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =  "-1004472965009"  # just put channel id dont add ""
+DB_CHANNEL =  "-1002704790611"  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
@@ -33,7 +33,7 @@ DB_CHANNEL =  "-1004472965009"  # just put channel id dont add ""
 # Auto Delete Timer (seconds)
 AUTO_DEL = 600
 # Admin IDs
-ADMINS = [6497757690, 6103092779]
+ADMINS = [7258467415, 7908181216]
 # Bot Settings
 DISABLE_BTN = True
 PROTECT = True
